b5cc68e30043b6178ecb7a24744dd68bafd57e09
Hoàng Trung <trung.nguyencasau@gmail.com>
2021-08-01 16:09:50 +0700

    + Fix bug of not showing corpses properly.\n+ Update readme

--- Classes/scenes/GameScene/components/enemy/Death.h
+++ Classes/scenes/GameScene/components/enemy/Death.h
@@ -11,28 +11,31 @@ public:
         // Create sprite and scale
         auto node = Sprite::create("sprites/enemy/death.png");
         node->setScale(scale);
+        node->setAnchorPoint(Vec2(0.5f, 0.0f));
 
         auto visibleSize = Director::getInstance()->getVisibleSize();
         Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-        // Set position
-        node->setPosition(Vec2(x, visibleSize.height / 4 + node->getContentSize().height * node->getScaleX() / 2 + origin.y));
-        CCLOG("Pos: %f %f\n", node->getPosition().x, node->getPosition().x);
-
         // Draw node on scene
-        scene->addChild(node, 2);
+        scene->addChild(node);
+        node->setGlobalZOrder(3.0f);
+
+        // Set position
+        node->setPosition(Vec2(x, visibleSize.height / 4 + origin.y));
+        CCLOG("Pos: %f %f\n", node->getPosition().x, node->getPosition().x);
 
         // Set animation
         node->runAction(MoveBy::create(ENEMY_APPROACHING_CYCLE, Vec2(0 - 1.5 * visibleSize.width, 0)));
+        node->runAction(Sequence::create(DelayTime::create(5.0f), RemoveSelf::create(), nullptr));
         CCLOG("After: %f %f\n", node->getPosition().x, node->getPosition().x);
 
-        // Create physic body
-        auto body = PhysicsBody::createBox(node->getContentSize());
-        body->setDynamic(false);
-        body->setCollisionBitmask(DEATH_BITMASK);
-        body->setContactTestBitmask(DESTROY_BITMASK);
-        body->setCategoryBitmask(DEATH_BITMASK);
-        node->setPhysicsBody(body); 
+        // // Create physic body
+        // auto body = PhysicsBody::createBox(node->getContentSize());
+        // body->setDynamic(false);
+        // body->setCollisionBitmask(DEATH_BITMASK);
+        // body->setContactTestBitmask(DESTROY_BITMASK);
+        // body->setCategoryBitmask(DEATH_BITMASK);
+        // node->setPhysicsBody(body); 
     }
 
     // static void release(Node* node) {
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# bloodymouse
-A game for Mashup Game Jam 2
+# About Bloody Mouse:
+
+Bloody Mouse is a game made for [Mashup Game Jam 2](https://itch.io/jam/mashup-game-jam-2). The game and all the art are made within 72 hours of the game jam.
+
+Main post: [https://friedcroco070801.itch.io/bloody-mouse](https://friedcroco070801.itch.io/bloody-mouse)
+
+# System requirement:
+
+This game is designed to run on Windows 8.0+.
+
+No further information about the systems is required.
+
+# Some Screenshots:
+<p align="center">
+<img src="Screenshots/screen1.png" width="480" height="270">
+</p>
+<p align="center">
+<img src="Screenshots/screen2.png" width="480" height="270">
+</p>
+
+# Download:
+
+Main post: [https://friedcroco070801.itch.io/bloody-mouse](https://friedcroco070801.itch.io/bloody-mouse)
+
+# Technology:
+
+C++/Cocos2d-x 4.0
